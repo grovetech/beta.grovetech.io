4ab71db2bc7b9f04ace383feb3460914cbb65817
Jon Brown <jon@grovetech.co>
2023-01-07 14:40:23 -0500

    New AppCast Feeds

---
+++ appcasts/ms_appcast.php
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle" xmlns:dc="http://purl.org/dc/elements/1.1/" version="2.0">
+    <channel>
+        <title>Grove Support Changelog</title>
+        <link>https://shine.grovedesigns.co/files/support/</link>
+        <description>Most recent changes with links to updates.</description>
+        <generator>Feeder 4.4.1(5892) Mac App Store Version; Mac OS X Version 12.6.1 (Build 21G217) https://reinventedsoftware.com/feeder/</generator>
+        <docs>http://blogs.law.harvard.edu/tech/rss</docs>
+        <language>en</language>
+        <pubDate>Sat, 07 Jan 2023 14:37:10 -0500</pubDate>
+        <lastBuildDate>Sat, 07 Jan 2023 14:37:10 -0500</lastBuildDate>
+        <atom:link href="https://grovetech.co/appcasts/ms_appcast.php" rel="self" type="application/rss+xml"/>
+        <item>
+            <title>Grove Support Version 8.5.0</title>
+            <description><![CDATA[Security Updates]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/GroveSupport85.zip" length="2183914" type="application/zip" sparkle:dsaSignature="MCwCFG1eMDhNScu5O+FiJgvGZQQy9h3cAhQORW0grhSEuTavmwQuSFiau3ZBcA==" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">E624AF5D-CFCB-43EE-B87D-C2B491B999B5</guid>
+            <sparkle:version>8.5</sparkle:version>
+            <sparkle:shortVersionString>Version 8.5.0</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 8.4.0</title>
+            <description><![CDATA[<p>macOS Monterey Support&lt;br&gt;</p>
+
+<p>Bug Fixes&lt;br&gt;</p>
+
+<p>M1 Support</p>
+]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/GroveSupport-8.4.zip" length="1840755" type="application/zip" sparkle:dsaSignature="MC0CFFAseudv+lmGgkmE5h8rq5qosv4/AhUAnWZt/Ex+IKGajd+MNjkfJYjZdSY=" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">EE32A606-A77A-4DDA-B19B-97108FD54B81</guid>
+            <sparkle:version>8.4</sparkle:version>
+            <sparkle:shortVersionString>Version 8.4.0</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 8.1.0</title>
+            <description><![CDATA[- Bug Fixes]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/GroveSupport-81.zip" length="8734062" type="application/zip" sparkle:dsaSignature="MCwCFE73fTuUzHkx6Pmp/J6SJ34A5MvZAhRdmdt03JKskBfjfJIqI2NZA45GqA==" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">F96CA769-68E8-45F2-9A1E-A4BC26D21A90</guid>
+            <sparkle:version>8.1</sparkle:version>
+            <sparkle:shortVersionString>Version 8.1.0</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 8.0.8</title>
+            <description><![CDATA[- Changed calendar system]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/Grove%20Support-8.0.8.zip" length="8926448" type="application/zip" sparkle:dsaSignature="MC0CFGhhk+lcswv8eipoHthtpk9TDvnyAhUAlpC0j9BWxrPcFiIwvujvw0XcykA=" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">9368C77B-2EA2-4351-B5D4-165058672A46</guid>
+            <sparkle:version>8.0.8</sparkle:version>
+            <sparkle:shortVersionString>Version 8.0.8</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 8.0.7</title>
+            <description><![CDATA[<p>- Fixed issue with Autolayout&lt;br&gt;</p>
+
+<p>- Fixed issue with Dark Mode</p>
+]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/Grove-Support-807.zip" length="8926399" type="application/octet-stream" sparkle:dsaSignature="MC0CFQCQiZMQ6aLi4kzNnCGOBJlU5PZShQIUG0iggjQ2MIQXrcfJlabQ4PVvcNo=" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">631B050D-D901-4C8F-BC07-E7D0E595B053</guid>
+            <sparkle:version>8.0.7</sparkle:version>
+            <sparkle:shortVersionString>Version 8.0.7</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 8.0.6</title>
+            <description><![CDATA[<p>- New redesigned UI for MacOS 10.14+&lt;br&gt;</p>
+
+<p>- Started Notarizing the App for MacOS 10.14.5&lt;br&gt;</p>
+
+<p>- Fixed minor UI issue&lt;br&gt;</p>
+
+<p>- Completely re-written from ground up in Swift 5&lt;br&gt;</p>
+
+<p>- Adds Launch on Login capabilities&lt;br&gt;</p>
+
+<p>- Fixes unknown developer issue on some computers</p>
+]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/Grove%20Support%208.0.6.app.zip" length="9170525" type="application/zip" sparkle:dsaSignature="MC0CFG4P/VJtVU1H592mYgTWK5PetoTWAhUAnRZhWa0GU74pOL1Q7T1qoLd76Nc=" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">EE6F87DE-0571-4E10-97F0-91ADA5A837B2</guid>
+            <sparkle:version>8.0.6</sparkle:version>
+            <sparkle:shortVersionString>Version 8.0.6</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 7.0.4</title>
+            <description><![CDATA[Bug Fixes]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/Mac%20Support-7.0.4.app.zip" length="4975163" type="application/zip" sparkle:dsaSignature="MCwCFGVV/TrFVzlH985nRFYjENFojdPbAhQ5LXB3Uc1hlvTagO0rAS1H3F5FQg==" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">56D698BE-1EB8-4F9B-BEE6-BA399D050BDB</guid>
+            <sparkle:version>7.0.4</sparkle:version>
+            <sparkle:shortVersionString>Version 7.0.4</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 7.0.3</title>
+            <description><![CDATA[- Added new contact and ticket intake form]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/Mac%20Support-7.0.3.app.zip" length="4974927" type="application/zip" sparkle:dsaSignature="MCwCFGxuEccjNVYprJ9Oht7BhrWc7q5jAhQR2Ybn3vwwb3IDbAYcXo+PgmuqCw==" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">9FC2257C-BBE2-4A60-96E6-806C6D5F44AB</guid>
+            <sparkle:version>7.0.3</sparkle:version>
+            <sparkle:shortVersionString>Version 7.0.3</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 7.0.2</title>
+            <description><![CDATA[- Bug Fixes]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/Mac%20Support.app.zip" length="4978224" type="application/zip" sparkle:dsaSignature="MCwCFGRh62s12+ZV1B+7qZMP+jLEjz+qAhQTcx77HWpCfyPi8/dMjejB+w5rmw==" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">BA7C2086-A8D7-4844-9024-7185A5979541</guid>
+            <sparkle:version>7.0.2</sparkle:version>
+            <sparkle:shortVersionString>Version 7.0.2</sparkle:shortVersionString>
+        </item>
+        <item>
+            <title>Grove Support Version 7.0.1</title>
+            <description><![CDATA[- Dark Mode Support 10.13 &amp; 10.14]]></description>
+            <enclosure url="https://shine.grovedesigns.co/files/support/MacSupport_701.zip" length="4978203" type="application/octet-stream" sparkle:dsaSignature="MCwCFH8qQ1PXDHSPPAmuecVOOp3vfxzlAhRE0LeWGoa66qy3pDO08zbaFIZUsQ==" sparkle:edSignature="tJyp84fF2B3xzrqlcRmpdkJ6alzvidg68PZJIRwfiU9fwP1KwkHZ04bOnUss50Z+EMxEJcxBtUEHV3sQGVx0Dw=="/>
+            <guid isPermaLink="false">0248035E-BE6E-4740-BCE9-9D43C79B8D1B</guid>
+            <sparkle:version>7.0.1</sparkle:version>
+            <sparkle:shortVersionString>Version 7.0.1</sparkle:shortVersionString>
+        </item>
+    </channel>
+</rss>
